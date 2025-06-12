6e6740305b3d92e7008c614c889a796f222b57ac
leowright <leow91863@gmail.com>
2025-06-11 20:22:57 -0600

    fix

---
+++ build/static/js/14.fd05ad5d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_react_app=self.webpackChunkmy_react_app||[]).push([[14],{4014:(e,a,p)=>{p.r(a),p.d(a,{default:()=>c});const c={}}}]);
+//# sourceMappingURL=14.fd05ad5d.chunk.js.map
---
+++ build/static/js/874.a6ad1c5f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_react_app=self.webpackChunkmy_react_app||[]).push([[874],{3874:(e,a,p)=>{p.r(a),p.d(a,{default:()=>c});const c={}}}]);
+//# sourceMappingURL=874.a6ad1c5f.chunk.js.map
