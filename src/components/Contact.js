ec055887870062d212e2dfa2b1503ccb1714f9fe
Hiroshi Saito <leow91863@gmail.com>
2025-07-02 11:56:39 -0600

    updated contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -80,7 +80,7 @@ const Contact = () => {
                       d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                     />
                   </svg>
-                  <span className="text-gray-600">leow91863@gmail.com</span>
+                  <span className="text-gray-600">rober.dev9@outlook.com</span>
                 </div>
                 <div className="flex items-center space-x-4">
                   <svg
@@ -96,7 +96,7 @@ const Contact = () => {
                       d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                     />
                   </svg>
-                  <span className="text-gray-600">+1 234 567 890</span>
+                  <span className="text-gray-600">*********</span>
                 </div>
               </div>
               <div className="mt-8">
